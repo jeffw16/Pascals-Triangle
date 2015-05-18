436515027795d1df86baf2cef8b00e64fb226dc4
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-05-18 14:42:02 -0500

    Create PascalsTriangle.java

---
+++ PascalsTriangle.java
@@ -0,0 +1,13 @@
+/**
+ * Pascal's Triangle
+ * @author Jeffrey Wang
+ * @license - may be used for educational purposes ONLY, excluding editing of this code
+*/
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+public static void main ( String[] args ) {
+  Scanner s = new Scanner ( System.in );
+  System.out.println ( "Pascal's Triangle" );
+}
